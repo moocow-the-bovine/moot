fd764709b8fa898eaa1be5a5c9e37230591dedc1
Bryan Jurish <jurish@bbaw.de>
2002-10-29 09:07:53 +0000

    + added shell-variables for autotools to autogen.sh
    git-svn-id: file:///home/svn/dev/moot/trunk@78 4a515de8-beb3-11dc-84d6-2b654028afac

--- autogen.sh
+++ autogen.sh
@@ -11,31 +11,36 @@ MY_AHDIRS="."
 MY_AMDIRS="."
 MY_ACDIRS="."
 
+test -z "$ACLOCAL" && ACLOCAL=aclocal
+test -z "$AUTOHEADER" && AUTOHEADER=autoheader
+test -z "$AUTOMAKE" && AUTOMAKE=automake
+test -z "$AUTOCONF" && AUTOCONF=autoconf
+
 if test -n "$MY_ALDIRS"; then
  for d in $MY_ALDIRS ; do
-    echo "(cd $d ; aclocal)"
-    (cd $d ; aclocal)
+    echo "(cd $d ; $ACLOCAL)"
+    (cd $d ; $ACLOCAL)
  done
 fi
 
 if test -n "$MY_AHDIRS"; then
  for d in $MY_AHDIRS ; do
-    echo "(cd $d ; autoheader)"
-    (cd $d ; autoheader)
+    echo "(cd $d ; $AUTOHEADER)"
+    (cd $d ; $AUTOHEADER)
  done
 fi
 
 if test -n "$MY_AMDIRS"; then
  for d in $MY_AMDIRS ; do
-    echo "(cd $d ; automake -a)"
-    (cd $d ; automake -a)
+    echo "(cd $d ; $AUTOMAKE -a)"
+    (cd $d ; $AUTOMAKE -a)
  done
 fi
 
 if test -n "$MY_ACDIRS"; then
  for d in $MY_ACDIRS ; do
-    echo "(cd $d ; autoconf)"
-    (cd $d ; autoconf)
+    echo "(cd $d ; $AUTOCONF)"
+    (cd $d ; $AUTOCONF)
  done
 fi
 
