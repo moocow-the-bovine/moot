3e5177c3a5acf9ba43f99001dee9272270f04c5d
Bryan Jurish <jurish@bbaw.de>
2003-07-16 10:56:59 +0000

    + library changes, workup to HMM tagger/disambiguator
    git-svn-id: file:///home/svn/dev/moot/trunk@140 4a515de8-beb3-11dc-84d6-2b654028afac

---
+++ src/dwdstvars.sh
@@ -0,0 +1,22 @@
+uname=`uname -n`
+
+case "$uname" in
+    "carrot")
+       fsm_base=/usr/local
+       bumble_base=/usr
+       stlp_base=/usr/local
+       ;;
+     *)
+       fsm_base=/usr/local
+       bumble_base=/usr/local
+       stlp_base=/usr
+       ;;
+esac
+      
+dwdst_cflags="-DHAVE_CONFIG_H -I. -I/usr/local/include -I${bumble_base}/include/bumble -I${fsm_base}/include/FSM -I${stlp_base}/include/stlport -I${fsm_base}/include/FSMext -Wall -ftemplate-depth-24"
+
+dwdst_oflags="-O3 -fomit-frame-pointer -funroll-loops -finline-limit-100000"
+
+dwdst_ldflags="-L/usr/local/lib"
+
+dwdst_libs='-lFSMext -lFSM -lFSMSymSpec -lstlport_gcc -lpthread -lz -lbumble++'
