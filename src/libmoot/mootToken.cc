d03b753c20ad614e8b2900df8e20aa6452f59b45
Bryan Jurish <jurish@bbaw.de>
2004-06-16 07:37:31 +0000

    + added mootToken.cc for safety (avoid segfaults)
    git-svn-id: file:///home/svn/dev/moot/trunk@264 4a515de8-beb3-11dc-84d6-2b654028afac

---
+++ src/libmoot/mootToken.cc
@@ -0,0 +1,72 @@
+/* -*- Mode: C++ -*- */
+
+/*
+   libmoot : moocow's part-of-speech tagging library
+   Copyright (C) 2003-2004 by Bryan Jurish <moocow@ling.uni-potsdam.de>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+*/
+
+/*--------------------------------------------------------------------------
+ * File: mootToken.cc
+ * Author: Bryan Jurish <moocow@ling.uni-potsdam.de>
+ * Description:
+ *   + moocow's PoS tagger : token information
+ *--------------------------------------------------------------------------*/
+
+#include "mootToken.h"
+
+#include <ctype.h>
+#include <list>
+#include <set>
+#include <string>
+
+
+#if defined(__GNUC__)
+# include <float.h>
+#endif // __GNUC__
+
+namespace moot {
+  using namespace std;
+
+  const char *mootTokFlavorNames[TF_NFLAVORS] =
+    {
+      "TF_UNKNOWN",
+      "TF_TOKEN",
+      "TF_COMMENT",
+      "TF_EOS",
+      "TF_EOF",
+      /*-- internal lexer use only */
+      "TF_TEXT",
+      "TF_TAB",
+      "TF_TAG",
+      "TF_DETAILS",
+      "TF_COST",
+      "TF_NEWLINE",
+      "TF_IGNORE"
+    };
+
+  const char *TokenTypeNames[NTokTypes] =
+    {
+      "@ALPHA",
+      "@CARD",
+      "@CARDPUNCT",
+      "@CARDSUFFIX",
+      "@CARDSEPS",
+      "@UNKNOWN"
+    };
+
+
+}; // moot_END_NAMESPACE
