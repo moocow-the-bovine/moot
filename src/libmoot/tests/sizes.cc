fb9f766ee41e4bcd3c4271ee6c76fab996895ae3
Bryan Jurish <jurish@bbaw.de>
2004-07-18 12:16:15 +0000

    + small doc changes
    git-svn-id: file:///home/svn/dev/moot/trunk@294 4a515de8-beb3-11dc-84d6-2b654028afac

---
+++ src/libmoot/tests/sizes.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void) {
+  printf("sizeof(int)    = %u\n", sizeof(int));
+  printf("sizeof(long)   = %u\n", sizeof(long));
+  printf("sizeof(float)  = %u\n", sizeof(float));
+  printf("sizeof(double) = %u\n", sizeof(double));
+  return 0;
+}
