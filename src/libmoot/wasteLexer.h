8ba51a698eecc9f256e0868a3df572d529839499
Bryan Jurish <jurish@bbaw.de>
2013-10-25 07:10:19 +0000

    + wasteLexer: some docs
    git-svn-id: file:///home/svn/dev/moot/trunk@10948 4a515de8-beb3-11dc-84d6-2b654028afac

--- src/libmoot/wasteLexer.h
+++ src/libmoot/wasteLexer.h
@@ -1,4 +1,22 @@
-/* -*- Mode: C++ -*- */
+/* -*- Mode: C++; coding: utf-8; c-basic-offset: 2; -*- */
+/*
+   libmoot : moot part-of-speech tagging library
+   Copyright (C) 2013 by Bryan Jurish <moocow@cpan.org> and Kay-Michael Würzner
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 /**
  *
@@ -26,21 +44,57 @@
 
 namespace moot
 {
+
+  /*============================================================================
+   * wasteLexicon
+   */
+  /** \brief simple hash_set<>-based lexicon class  */
   class wasteLexicon
   {
-    public:
-      typedef moot_STL_NAMESPACE::hash_set<std::string>  Lexicon;
-      Lexicon lex;
+  public:
+    typedef moot_STL_NAMESPACE::hash_set<std::string>  Lexicon;  /**< typedef for underlying lexicon data */
+    Lexicon lex;  /**< underlying lexicon data */
 
-      wasteLexicon();
-      ~wasteLexicon();
+    //------------------------------------------------------------
+    /// \name Constructors etc.
+    //@{
+    /** Default constructor */
+    wasteLexicon();
 
-      void load(mistream *mis);
-      void load(const char *filename);
-      void clear();
-      bool lookup(const std::string &word);
+    /** desctructor */
+    ~wasteLexicon();
+
+    /** clear all entries */
+    void clear();
+    //@}
+
+    //------------------------------------------------------------
+    /// \name Access
+    //@{
+    /** \returns true iff \a word is present in the lexicon */
+    bool lookup(const std::string &word) const;
+    //@}
+
+    //------------------------------------------------------------
+    /// \name I/O
+    //@{
+    /** load lexicon from a moot::TokenReader: adds only mootToken::text() of moot::TokTypeVanilla tokens */
+    void load(moot::TokenReader *reader);
+
+    /** load lexicon from a \a mootio::mistream; wraps load(moot::TokenReader*) using a temporary TokenReaderNative */
+    void load(mootio::mistream *mis);
+
+    /** load lexicon from a named file; wraps load(mootoi::mistream*) method */
+    void load(const char *filename);
+    //@}
   };
 
+  /*============================================================================
+   * wasteLexer
+   */
+  /** \brief Mid-level TokenReader scanner stage
+   *  \detail performs (optional) hyphenation normalization and text classification
+   */
   class wasteLexer : public TokenReader
   {
     public:
